d538efa18316e7dc1a8e694cfd13c777a2278a0a
fral22uh <fral22uh@student.ju.se>
2024-09-01 14:07:49 +0200

    New change to calculator.py

--- BE/calculator.py
+++ BE/calculator.py
@@ -45,15 +45,15 @@ if not args.rest and len(sys.argv) != 4:
 
 if (args.add):
     result = CalculatorHelper().add(args.add[0], args.add[1])
-    print(f'{args.add[0]}+{args.add[1]}={result}')
+    print(f'{args.add[0]}+{args.add[1]}={result} Here is an added string')
 elif (args.subtract):
     result = CalculatorHelper().subtract(args.subtract[0], args.subtract[1])
-    print(f'{args.subtract[0]}-{args.subtract[1]}={result}')
+    print(f'{args.subtract[0]}-{args.subtract[1]}={result} Here is an added string')
 elif (args.multiply):
     result = CalculatorHelper().multiply(args.multiply[0], args.multiply[1])
-    print(f'{args.multiply[0]}*{args.multiply[1]}={result}')
+    print(f'{args.multiply[0]}*{args.multiply[1]}={result} Here is an added string')
 elif (args.divide):
     result = CalculatorHelper().divide(args.divide[0], args.divide[1])
-    print(f'{args.divide[0]}/{args.divide[1]}={result}')
+    print(f'{args.divide[0]}/{args.divide[1]}={result} Here is an added string')
 elif (args.rest):
     calculator_rest_service.main(args)
